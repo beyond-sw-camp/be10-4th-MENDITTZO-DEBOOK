6751390024ae10d8d944af88879bac62a6bf5f45
enking <engur9902@gmail.com>
2024-11-04 12:23:49 +0900

    feat: elastic search service

--- MENDITTZO-Backend/src/main/java/com/mendittzo/elastic/query/service/ElasticService.java
+++ MENDITTZO-Backend/src/main/java/com/mendittzo/elastic/query/service/ElasticService.java
@@ -1,4 +1,22 @@
 package com.mendittzo.elastic.query.service;
 
+import com.mendittzo.elastic.query.dto.ElasticDTO;
+import com.mendittzo.elastic.query.repository.ElasticRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ElasticService {
+    private final ElasticRepository elasticRepository;
+
+    @Autowired
+    public ElasticService(ElasticRepository elasticRepository) {
+        this.elasticRepository = elasticRepository;
+    }
+
+    public List<ElasticDTO> searchByTitle(String keyword) {// 자동완성 및 부분 검색 기능
+        return elasticRepository.findByTitleContaining(keyword);
+    }
 }
