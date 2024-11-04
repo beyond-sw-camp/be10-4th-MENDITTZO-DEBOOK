09e99c77170b61ff162fb7d5767d6820b3995e8f
enking <engur9902@gmail.com>
2024-11-04 12:36:04 +0900

    feat: elastic search controller 구성

--- MENDITTZO-Backend/src/main/java/com/mendittzo/elastic/query/controller/ElasticController.java
+++ MENDITTZO-Backend/src/main/java/com/mendittzo/elastic/query/controller/ElasticController.java
@@ -1,4 +1,31 @@
 package com.mendittzo.elastic.query.controller;
 
+import com.mendittzo.elastic.query.dto.ElasticDTO;
+import com.mendittzo.elastic.query.service.ElasticService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/elastic")
 public class ElasticController {
+
+    private final ElasticService elasticService;
+
+    @Autowired
+    public ElasticController(ElasticService elasticService) {
+        this.elasticService = elasticService;
+    }
+
+    // GET 요청을 통해 자동완성 및 부분 검색 기능을 제공하는 엔드포인트
+    @GetMapping("/autocomplete")
+    public List<ElasticDTO> autocomplete(@RequestParam String query) {
+        return elasticService.searchByTitle(query); // Service의 메서드를 호출하여 검색 결과를 반환
+    }
+
+
 }
