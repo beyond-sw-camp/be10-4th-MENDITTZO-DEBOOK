c65d4979f8b4800c93451923bb84ff1157952e2f
eunseo-76 <eunseolee19@gmail.com>
2024-11-08 15:34:11 +0900

    feat: 로그아웃 구현

--- MENDITTZO-Backend/src/main/java/com/mendittzo/auth/command/application/controller/KakaoAuthController.java
+++ MENDITTZO-Backend/src/main/java/com/mendittzo/auth/command/application/controller/KakaoAuthController.java
@@ -8,14 +8,17 @@ import com.mendittzo.auth.query.application.dto.DebookTokenDTO;
 import com.mendittzo.auth.query.application.service.TokenService;
 import com.mendittzo.common.exception.CustomException;
 import com.mendittzo.common.exception.ErrorCode;
+import com.mendittzo.common.exception.SuccessCode;
 import com.mendittzo.security.util.JwtUtil;
+import com.mendittzo.security.util.UserUtil;
+import com.mendittzo.user.command.domain.aggregate.User;
+import com.mendittzo.user.command.domain.repository.UserRepository;
+import com.mendittzo.user.query.application.dto.UserQueryResponseDTO;
+import com.mendittzo.user.query.application.service.UserQueryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,14 +26,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class KakaoAuthController {
 
     private final KakaoAuthService kakaoAuthService;
+    private final JwtUtil jwtUtil;
+    private final UserRepository userRepository;
+    private final TokenService tokenService;
 
     // 새로운 액세스 토큰 발급하기
     @PostMapping("/access-tokens")
     public ResponseEntity<AccessTokenResponseDTO> refreshAccessToken(@RequestHeader("Authorization") String authorizationHeader) {
 
-        String refreshToken = authorizationHeader.replace("Bearer ", "");
-        AccessTokenResponseDTO AccessTokenResponse = kakaoAuthService.sendNewAccessToken(new RefreshTokenRequestDTO(refreshToken));
+        AccessTokenResponseDTO newAccessToken = kakaoAuthService.sendNewAccessToken(authorizationHeader);
 
-        return new ResponseEntity<>(AccessTokenResponse, HttpStatus.OK);
+        return new ResponseEntity<>(newAccessToken, HttpStatus.OK);
+    }
+
+    // 로그아웃
+    @DeleteMapping("/logout")
+    public ResponseEntity<String> logout(@RequestHeader("Authorization") String authorizationHeader) {
+
+        kakaoAuthService.logoutFromDebook(authorizationHeader);
+
+        return ResponseEntity.ok(SuccessCode.SUCCESS.getMessage());
     }
 }
