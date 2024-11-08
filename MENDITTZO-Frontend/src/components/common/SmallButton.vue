fd5eafce52d00d8ec2ef9051a9de64486aa43214
kimjm9911 <kimjm9911@naver.com>
2024-11-08 17:35:43 +0900

    feat: #15 버튼, 드롭다운 컴포넌트 생성

---
+++ MENDITTZO-Frontend/src/components/common/SmallButton.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { defineEmits } from 'vue';
+
+const emit = defineEmits(['click']);
+</script>
+
+<template>
+  <button class="btn" @click="emit('click')">
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+  .btn {
+    width: 80px;
+    height: 30px;
+    background-color: #78AE6B;
+    color: white;
+    border: none;
+    border-radius: 10px;
+    cursor: pointer;
+    font-size: 14px;
+    font-weight: bold;
+  }
+</style>
