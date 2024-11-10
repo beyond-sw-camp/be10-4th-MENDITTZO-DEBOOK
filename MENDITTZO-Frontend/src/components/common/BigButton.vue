58c7b75020bbd1c4cb42442a3f8b6051949a77c1
kimjm9911 <kimjm9911@naver.com>
2024-11-10 17:23:18 +0900

    chore: #24 비활성화 상태 추가

--- MENDITTZO-Frontend/src/components/common/BigButton.vue
+++ MENDITTZO-Frontend/src/components/common/BigButton.vue
@@ -1,12 +1,31 @@
 <script setup>
-import { defineEmits } from 'vue';
+import { defineProps, defineEmits } from 'vue';
 
 const emit = defineEmits(['click']);
+
+// props로 disabled를 받아옵니다.
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false
+  }
+});
+
+// 버튼 클릭 처리
+const handleClick = () => {
+  if (!props.disabled) {
+    emit('click');
+  }
+};
 </script>
 
 <template>
-  <button class="btn" @click="emit('click')">
-    <slot></slot>
+  <button
+      class="btn"
+      :disabled="props.disabled"
+      @click="handleClick"
+  >
+  <slot></slot>
   </button>
 </template>
 
@@ -19,7 +38,12 @@ const emit = defineEmits(['click']);
   border: none;
   border-radius: 10px;
   cursor: pointer;
-  font-size: 23px;
+  font-size: 20px;
   font-weight: bold;
+}
+
+.btn:disabled {
+  background-color: #BDBDBD;  /* 비활성화된 상태의 배경색 */
+  cursor: not-allowed;         /* 비활성화된 상태에서 커서 변경 */
 }
 </style>
