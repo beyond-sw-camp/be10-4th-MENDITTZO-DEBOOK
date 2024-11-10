97d2d827f97cd6279809aaedb37512a000c95f2b
enking <engur9902@gmail.com>
2024-11-10 20:31:51 +0900

    feat:ElasticService test code 인덱싱 되는지 확인하는 테스트 코드

--- MENDITTZO-Backend/src/test/java/com/mendittzo/elastic/query/service/ElasticServiceTest.java
+++ MENDITTZO-Backend/src/test/java/com/mendittzo/elastic/query/service/ElasticServiceTest.java
@@ -1,4 +1,17 @@
-import static org.junit.jupiter.api.Assertions.*;
-class ElasticServiceTest {
-  
+package com.mendittzo.elastic.query.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ElasticServiceTest {
+
+    @Autowired
+    private ElasticService elasticService;
+
+    @Test
+    public void testStartIndexing() {
+        elasticService.startIndexing();  // 수동으로 인덱싱 실행 되는지 여부 확인
+    }
 }
