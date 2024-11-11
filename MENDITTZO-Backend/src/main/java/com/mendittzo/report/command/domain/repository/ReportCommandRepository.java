140940515ab193eeb627e2f6a431f89f9feb5c47
JungUiJin <yealkki38@gmail.com>
2024-11-12 02:02:06 +0900

    chore: #70 오류 해결

--- MENDITTZO-Backend/src/main/java/com/mendittzo/report/command/domain/repository/ReportCommandRepository.java
+++ MENDITTZO-Backend/src/main/java/com/mendittzo/report/command/domain/repository/ReportCommandRepository.java
@@ -14,6 +14,4 @@ public interface ReportCommandRepository {
     List<Report> findAllByChatroomId(Long chatroomId);
 
     List<Report> findAllByChatId(String chatId);
-
-    Report findByReporterUser(Long reporterLoginId);
 }
