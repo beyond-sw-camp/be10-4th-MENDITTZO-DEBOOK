c1084b0c00bd2c4a769806130f28b08eb5f4991b
kimjm9911 <kimjm9911@naver.com>
2024-11-11 16:09:43 +0900

    chore: #71 수정시간 나오도록 수정

--- MENDITTZO-Backend/src/main/java/com/mendittzo/review/query/domain/repository/ReviewQueryRepository.java
+++ MENDITTZO-Backend/src/main/java/com/mendittzo/review/query/domain/repository/ReviewQueryRepository.java
@@ -11,15 +11,15 @@ import java.util.Optional;
 
 public interface ReviewQueryRepository extends JpaRepository<Review, Long> {
 
-    @Query("SELECT new com.mendittzo.review.query.application.dto.ReviewResponseDTO(r.reviewId, r.title, r.content, r.rating, u.nickname, r.createDatetime, r.userId) " +
+    @Query("SELECT new com.mendittzo.review.query.application.dto.ReviewResponseDTO(r.reviewId, r.title, r.content, r.rating, u.nickname, r.createDatetime, r.updateDatetime, r.userId, r.bookId) " +
             "FROM Review r JOIN User u ON r.userId = u.userId  WHERE r.bookId = :bookId AND NOT r.status = 'DELETED'")
     Page<ReviewResponseDTO> findReviewsByBookId(Long bookId, Pageable pageable);
 
-    @Query("SELECT new com.mendittzo.review.query.application.dto.ReviewResponseDTO(r.reviewId, r.title, r.content, r.rating, u.nickname, r.createDatetime, r.userId) " +
+    @Query("SELECT new com.mendittzo.review.query.application.dto.ReviewResponseDTO(r.reviewId, r.title, r.content, r.rating, u.nickname, r.createDatetime, r.updateDatetime, r.userId, r.bookId) " +
             "FROM Review r JOIN User u ON r.userId = u.userId  WHERE r.userId = :userId AND NOT r.status = 'DELETED'")
     Page<ReviewResponseDTO> findReviewsByUserId(Long userId, Pageable pageable);
 
-    @Query("SELECT new com.mendittzo.review.query.application.dto.ReviewResponseDTO(r.reviewId, r.title, r.content, r.rating, u.nickname, r.createDatetime, r.userId) " +
+    @Query("SELECT new com.mendittzo.review.query.application.dto.ReviewResponseDTO(r.reviewId, r.title, r.content, r.rating, u.nickname, r.createDatetime, r.updateDatetime, r.userId, r.bookId) " +
             "FROM Review r JOIN User u ON r.userId = u.userId WHERE r.reviewId = :reviewId AND NOT r.status = 'DELETED'")
     Optional<ReviewResponseDTO> findReviewByReviewId(Long reviewId);
 }
