5211fb2c6826b3546e23ca35e1f3118ed601b808
kimjm9911 <kimjm9911@naver.com>
2024-11-11 22:29:40 +0900

    chore: #34 페이지 이동 시 스크롤 맨 위로 이동

--- MENDITTZO-Frontend/src/router/router.js
+++ MENDITTZO-Frontend/src/router/router.js
@@ -1,7 +1,4 @@
 import {createRouter, createWebHistory} from "vue-router";
-import BookListView from "@/views/BookListView.vue";
-import BookDetailView from "@/views/BookDetailView.vue";
-import BookReviewCreateView from "@/views/BookReviewCreateView.vue";
 
 const router = createRouter({
     history: createWebHistory(),
@@ -43,7 +40,12 @@ const router = createRouter({
             path: '/booklists/:bookId/review/:reviewId/edit',
             component: () => import('@/views/BookReviewEditView.vue')
         }
-    ]
+    ],
+    scrollBehavior(to, from, savedPosition) {
+        // 스크롤을 항상 맨 위로 이동
+        return { top: 0 };
+    }
+
 });
 
 export default router;
